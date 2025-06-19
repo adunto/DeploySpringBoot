5da5ca455bf484a3ddc8ba88e9558c6966b11e5e
Kwon Soonmin <sogno6037@gmail.com>
2025-06-19 18:48:37 +0900

    test

--- src/test/java/com/employee/deploy/DeploySpringBootApplicationTests.java
+++ src/test/java/com/employee/deploy/DeploySpringBootApplicationTests.java
@@ -1,13 +1,12 @@
 package com.employee.deploy;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DeploySpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
